04bac13b3b69d3e7ba5cdcd4832e5f9675fb8493
sdnam <sdnam@sdnamui-MacBook-Pro.local>
2017-06-28 10:01:41 +0900

    first commit

---
+++ test1.py
@@ -0,0 +1,31 @@
+print("E")
+print(type("e"))
+print(type(1))
+
+print(type(['1','2','3']))
+
+list = ['A1','A2','A3']
+
+print('4' in list)
+
+print(len(list))
+list.append("A4")
+list.append("A5")
+
+# list.clear()
+
+print(list)
+del(list[0])
+print(list)
+
+for m in list :
+    print(m)
+
+print("\n")
+i = 0
+while i < len(list):
+    print(list[i])
+    i = i + 1
+
+
+for
